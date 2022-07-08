2dfc71ff91e25c2c907d6f379987880cad1cc3b7
lgramu97 <gramuglialautaro@gmail.com>
2022-07-07 21:29:01 -0300

    Create Execution Scheduler

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -21,7 +21,7 @@ def _training_model():
 
 def _choose_best_model(ti):
     #Get the values of the 3 tasks.
-    accuracies = ti.xcom_pull(task_id=[
+    accuracies = ti.xcom_pull(task_ids=[
         'training_model_A',
         'training_model_B',
         'training_model_C'
@@ -31,12 +31,12 @@ def _choose_best_model(ti):
     if (best_accuracy > 8):
         return 'accurate'
     return 'inaccurate'
-    
 
 #This direct acyclic graph (DAG) will start 2022-07-07 and execute daily.
 with DAG("my_dag",start_date=datetime(2022,7,7),
          schedule_interval="@daily",catchup=False,) as dag:
     
+    #Define Models
     traning_model_A = PythonOperator(
         task_id = "training_model_A",
         python_callable=_training_model        
@@ -52,11 +52,13 @@ with DAG("my_dag",start_date=datetime(2022,7,7),
         python_callable=_training_model        
     )
     
+    #Define Branch
     choose_best_model = BranchPythonOperator(
         task_id = "choose_best_model",
         python_callable=_choose_best_model
     )
     
+    #Define Bash Operators
     accurate = BashOperator(
         task_id = "accurate",
         bash_command="echo accurate"
@@ -67,3 +69,7 @@ with DAG("my_dag",start_date=datetime(2022,7,7),
         task_id = "inaccurate",
         bash_command="echo inaccurate"
     )
+    
+    #Same level models. Then choose best model. Finally choose action (same level)
+    [traning_model_A,traning_model_B,traning_model_C] >> choose_best_model >> [inaccurate, accurate]
+
